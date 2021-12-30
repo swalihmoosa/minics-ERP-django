edb9c635390581ebe2e9a99ff4ba37df35f9ed21
swalihmoosa <swalihmoosa72@gmail.com>
2021-12-30 10:46:15 +0530

    Cart section ongoing

--- product/admin.py
+++ product/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.db import models
 
-from product.models import ProductItem
+from product.models import Cart, ProductItem
 
 
 class ProductItemadmin(admin.ModelAdmin):
     list_display = ["product_name", "product_price", "product_image"]
 
 admin.site.register(ProductItem, ProductItemadmin)
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["product_name", "product_price", "product_image"]
+
+admin.site.register(Cart, CartAdmin)
--- product/models.py
+++ product/models.py
@@ -13,6 +13,18 @@ class ProductItem(models.Model):
         ordering = ["id"]
 
 
+class Cart(models.Model):
+    product_name = models.CharField(max_length=125)
+    product_price = models.CharField(max_length=125)
+    product_image = models.ImageField(upload_to="products/")
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        ordering = ["id"]
+
+
 
 
 
