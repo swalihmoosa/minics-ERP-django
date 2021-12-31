3e09773e2cd828f4088ddcbb48029828fb6c30ae
swalihmoosa <swalihmoosa72@gmail.com>
2021-12-31 16:54:21 +0530

    Cart section | django product-count

--- product/views.py
+++ product/views.py
@@ -26,10 +26,14 @@ def cart(request):
 def add_to_cart(request,pk):
     cart_product = ProductItem.objects.get(pk=pk)
 
-    Cart.objects.create(
-        product_name = cart_product.product_name, product_price = cart_product.product_price, product_image = cart_product.product_image
-    )
-
+    if not Cart.objects.filter(product_name=cart_product.product_name).exists():
+        Cart.objects.create(
+            product_name = cart_product.product_name, product_price = cart_product.product_price, product_image = cart_product.product_image
+        )
+    else:
+        cart_product = Cart.objects.get(product_name=cart_product.product_name)
+        cart_product.product_count +=1
+        cart_product.save()
     return redirect('/')
 
 
@@ -46,8 +50,6 @@ def add_count(request,pk):
     add_count_product.product_count += 1
     add_count_product.save()
 
-    print("##################################################3###############################",add_count_product)
-
     return redirect('/cart')
 
 
@@ -55,7 +57,5 @@ def minus_count(request,pk):
     minus_count_product = Cart.objects.get(pk=pk)
     minus_count_product.product_count -= 1
     minus_count_product.save()
-
-    print("##################################################3###############################",minus_count_product)
 
     return redirect('/cart')
