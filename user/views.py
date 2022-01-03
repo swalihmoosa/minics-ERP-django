6e9e23c186ea766a7420cd2e5f06a1db9c9b3db5
swalihmoosa <swalihmoosa72@gmail.com>
2022-01-03 17:42:55 +0530

    User section | signup

--- user/views.py
+++ user/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate
 
 
 def user_login(request):
@@ -8,10 +7,14 @@ def user_login(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
+        keep_signin = request.POST.get("keep_signin")
         user = authenticate(username=username, password=password)
         if user is not None:
             request.session['username'] = username
+            if keep_signin == "on":
+                SESSION_EXPIRE_SECONDS = 60
             return redirect('web:index')
+            
 
     if "username" in request.session:
         return redirect('web:index')
